db6c881983058f7f2a8a2a1db3c75de84ba726c3
Blaise Willoughby <blaise_willoughby@yahoo.com>
2021-06-27 14:26:58 -0500

    push to try AWS

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -10,54 +10,78 @@ class Navbar extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div className="container-fluid">
-                    <NavLink className="navbar-brand" to="/">giveBlaiseAnInterview</NavLink>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarNavAltMarkup"
-                        aria-controls="navbarNavAltMarkup"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <NavLink exact className="nav-link" to="/">About Me</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink exact className="nav-link" to="/sortingvisualizer">Sorting Visualizer</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink exact className="nav-link" to="/fullstackcomments">RESTful API</NavLink>
-                            </li>
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div className="container-fluid">
+                        <NavLink className="navbar-brand" to="/">giveBlaiseAnInterview</NavLink>
+                        <button
+                            className="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav #navbar"
+                            aria-controls="navbarNav"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <NavLink exact className="nav-link" to="/">About Me</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink exact className="nav-link" to="/sortingvisualizer">Sorting Visualizer</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink exact className="nav-link" to="/fullstackcomments">RESTful API</NavLink>
+                                </li>
 
-                        </ul>
+                            </ul>
 
-                        <div className="Navbar-buttons">
-                            <a className="resume" href="https://www.linkedin.com/in/blaise-willoughby-081368156/" without rel="noopener noreferrer" target="_blank">
-                                <button className="btn btn-success" trailingIcon="picture_as_pdf" label="Resume">
-                                    LinkedIn
-                                </button>
-                            </a>
-                            <a className="resume" href="https://github.com/BlaiseTheGreat" without rel="noopener noreferrer" target="_blank">
-                                <button className="btn btn-success" trailingIcon="picture_as_pdf" label="Resume">
-                                    Github
-                                </button>
-                            </a>
-                            <a className="resume" href={Pdf} without rel="noopener noreferrer" target="_blank">
-                                <button className="btn btn-success" trailingIcon="picture_as_pdf" label="Resume">
-                                    View Resume
-                                </button>
-                            </a>
+                            <div className="Navbar-buttons">
+                                <a className="resume" href="https://www.linkedin.com/in/blaise-willoughby-081368156/" without rel="noopener noreferrer" target="_blank">
+                                    <button className="btn btn-success" trailingIcon="picture_as_pdf" label="Resume">
+                                        LinkedIn
+                                    </button>
+                                </a>
+                                <a className="resume" href="https://github.com/BlaiseTheGreat" without rel="noopener noreferrer" target="_blank">
+                                    <button className="btn btn-success" trailingIcon="picture_as_pdf" label="Resume">
+                                        Github
+                                    </button>
+                                </a>
+                                <a className="resume" href={Pdf} without rel="noopener noreferrer" target="_blank">
+                                    <button className="btn btn-success" trailingIcon="picture_as_pdf" label="Resume">
+                                        View Resume
+                                    </button>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </nav>
+                </nav>
+
+
+                {/* <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="/">Navbar</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/">Features</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/">Pricing</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav> */}
+            </div>
         );
     }
 }
